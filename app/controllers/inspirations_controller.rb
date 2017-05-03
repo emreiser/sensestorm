8178271e62e5c1dcc0d426128908536d8614aae9
emreiser <emilie.reiser@gmail.com>
2017-05-03 15:12:16 -0400

    Switch image to jpg

--- app/controllers/inspirations_controller.rb
+++ app/controllers/inspirations_controller.rb
@@ -8,12 +8,13 @@ class InspirationsController < ApplicationController
   def create
     @inspiration = Inspiration.create!(inspiration_params)
     data = inspiration_params[:image]
-    filename = "#{Rails.root}/public/image_#{@inspiration.id}_#{DateTime.now.to_date}.png"
-    File.open(filename, 'wb') do |f|
-      f.write Base64.decode64(data['data:image/png;base64,'.length .. -1])
+    filename = "image_#{@inspiration.id}_#{DateTime.now.to_date}.jpg"
+
+    File.open("#{Rails.root}/public/#{filename}", 'wb') do |f|
+      f.write Base64.decode64(data['data:image/jpg;base64,'.length .. -1])
     end
 
-    @inspiration.image = filename
+    @inspiration.image = URI.join(root_url, filename).to_s
     @inspiration.save
     redirect_to action: "index"
   end
